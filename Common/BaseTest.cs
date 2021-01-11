7f48f9db1017a0e58b34a3bf2a38abdf5befaf7e
Jamile Agnes <jamileagnes@gmail.com>
2021-01-11 11:56:38 -0300

    iniciando repositorio

---
+++ Common/BaseTest.cs
@@ -0,0 +1,45 @@
+
+
+using System;
+using Coypu;
+using Coypu.Drivers.Selenium;
+using NinjaPlus.Lib;
+using NUnit.Framework;
+
+namespace NinjaPlus.Common
+{
+    public class BaseTest
+    {
+        protected BrowserSession Browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            var configs = new SessionConfiguration
+            {
+                AppHost = "http://ninjaplus-web",
+                Port = 5000,
+                SSL = false,
+                Driver = typeof(SeleniumWebDriver),
+                Browser = Coypu.Drivers.Browser.Chrome,
+                Timeout = TimeSpan.FromSeconds(10)
+            };
+
+            Browser = new BrowserSession(configs);
+
+            Browser.MaximiseWindow();
+        }
+
+        public string CoverPath()
+        {
+            var outputPath = Environment.CurrentDirectory;
+            return outputPath + "\\Images\\";
+        }
+
+        [TearDown]
+        public void Finish()
+        {
+            Browser.Dispose();
+        }
+    }
+}
